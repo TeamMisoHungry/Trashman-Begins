3d22570419d6b13f32c34ac24e3ac13ece9942b8
vmoudy <vpmoudy@gmail.com>
2015-02-25 13:21:35 -0800

    implemented the HUD and score system

--- Trashman/js/entities/HUD.js
+++ Trashman/js/entities/HUD.js
@@ -24,7 +24,7 @@ game.HUD.Container = me.Container.extend({
         this.name = "HUD";
 
         // add our child score object at the top left corner
-        this.addChild(new game.HUD.ScoreItem(5, 5));
+        this.addChild(new game.HUD.ScoreItem(630, 460));
     }
 });
 
@@ -41,7 +41,8 @@ game.HUD.ScoreItem = me.Renderable.extend({
         // call the parent constructor
         // (size does not matter here)
         this._super(me.Renderable, 'init', [x, y, 10, 10]);
-
+		this.font = new me.BitmapFont("32x32_font", 16);
+		this.font.set("right");
         // local copy of the global score
         this.score = -1;
     },
@@ -62,8 +63,8 @@ game.HUD.ScoreItem = me.Renderable.extend({
     /**
      * draw the score
      */
-    draw : function (context) {
-        // draw it baby !
+    draw : function (renderer) {
+        this.font.draw(renderer, game.data.score, this.pos.x, this.pos.y);
     }
 
 });
