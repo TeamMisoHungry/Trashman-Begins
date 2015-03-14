0cacbbf52322cfca9d5d55084823c07455020e51
Vi Tu <vttu@ucsc.edu>
2015-03-14 05:34:28 -0700

    FINAL PUSH

--- Trashman/js/screens/play.js
+++ Trashman/js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
      */
     onResetEvent: function() {
 		//load level
-		me.levelDirector.loadLevel("headquarter");
+		me.levelDirector.loadLevel("citypuzzle3");
 
 
         // reset the score
